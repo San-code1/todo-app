84bf93f43ecea82a1b80040362d87e971d5a2ea4
SanchezJavi <javi_sanchez_garcia@hotmail.com>
2025-12-12 19:09:12 +0100

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
-
-// Initialize Vercel Web Analytics on the client side
-inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics mode={import.meta.env.PROD ? 'production' : 'development'} />
   </StrictMode>,
 )
